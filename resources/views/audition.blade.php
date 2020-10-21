f5558a68df799d7b55168482482faa18c98e8268
starsidea <director_9414@outlook.com>
2020-10-21 19:11:55 +0800

    remote update

--- resources/views/audition.blade.php
+++ resources/views/audition.blade.php
@@ -11,8 +11,46 @@
         }
     </style>
 
-    <div class="container my-5">
+    <div class="container my-5 col-md-12">
         <h1 class="text-center">Audition</h1>
-        
+        <div class="col-md-4 upload-form-section">
+            <form id="audition file" method="post" action="#">
+                @csrf
+                <div class="form-group">
+                    <label for="name">Name:</label>
+                    <input type="text" class="form-control" placeholder="Enter name" id="name">
+                </div>
+                <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input type="text" class="form-control" placeholder="Enter email" id="email">
+                </div>
+                <div class="form-group">
+                    <label for="telephone">Telephone:</label>
+                    <input type="text" class="form-control" placeholder="Enter telephone" id="telephone">
+                </div>
+                <div class="form-group">
+                    <label for="band">Band / Stage Name:</label>
+                    <input type="text" class="form-control" placeholder="Enter band /stage name" id="band">
+                    <label class="form-check-label form-check">
+                        <input class="form-check-input" type="checkbox" name="check-brand"> Same as name
+                    </label>
+                </div>
+                <div class="form-group">
+                    <label for="genre">Genre:</label>
+                    <input type="text" class="form-control" placeholder="Enter genre" id="genre">
+                </div>
+                <div class="form-group">
+                    <label for="location">Location:</label>
+                    <input type="text" class="form-control" placeholder="Enter location" id="location">
+                </div>
+                <div class="form-group">
+                    <p>Disclaimer Text box (Display text) </p>
+                    <label class="form-check-label form-check">
+                        <input class="form-check-input" type="checkbox" name="disclaimer"> Accept disclaimer
+                    </label>
+                </div>
+                <button type="submit" class="btn btn-primary">Upload</button>
+            </form>
+        </div>
     </div>
 @endsection
