63746ac9018f1a9d58d9ca8ebff5cef13bf70e77
starsidea <director_9414@outlook.com>
2020-10-22 10:23:10 +0800

    test

--- resources/views/audition.blade.php
+++ resources/views/audition.blade.php
@@ -13,33 +13,33 @@
 
     <div class="container my-5 col-md-12">
         <h1 class="text-center">Audition</h1>
+        <div class="col-md-4 upload-form-section">
         @if (session('success'))
             <div class="alert alert-success alert-dismissable">
                 <button type="button" class="close" data-dismiss="alert">&times;</button>
                 {{session('success')}}
             </div>
         @endif
-        <div class="col-md-4 upload-form-section">
             <form action="{{ route('uploadfile') }}" enctype="multipart/form-data" method="post">
                 @csrf
                 <div class="form-group">
                     <label for="name">Name:</label>
-                    <input type="text" class="form-control" placeholder="Enter name" name="name" id="name">
+                    <input type="text" class="form-control" placeholder="Enter name" name="name" id="name" value="{{ old('name') }}">
                     <span class="help-block text-danger">{{$errors->first('name')}}</span>
                 </div>
                 <div class="form-group">
                     <label for="email">Email:</label>
-                    <input type="text" class="form-control" placeholder="Enter email" name="email" id="email">
+                    <input type="text" class="form-control" placeholder="Enter email" name="email" id="email" value="{{ old('email') }}">
                     <span class="help-block text-danger">{{$errors->first('email')}}</span>
                 </div>
                 <div class="form-group">
                     <label for="telephone">Telephone:</label>
-                    <input type="text" class="form-control" placeholder="Enter telephone" name="telephone" id="telephone">
+                    <input type="text" class="form-control" placeholder="Enter telephone" name="telephone" id="telephone" value="{{ old('telephone') }}">
                     <span class="help-block text-danger">{{$errors->first('telephone')}}</span>
                 </div>
                 <div class="form-group">
                     <label for="band">Band / Stage Name:</label>
-                    <input type="text" class="form-control" placeholder="Enter band /stage name" name="band" id="band">
+                    <input type="text" class="form-control" placeholder="Enter band /stage name" name="band" id="band" value="{{ old('band') }}">
                     <span class="help-block text-danger">{{$errors->first('band')}}</span>
                     <label class="form-check-label form-check">
                         <input class="form-check-input" type="checkbox" name="check-brand" id="check_brand"> Same as name
@@ -47,12 +47,12 @@
                 </div>
                 <div class="form-group">
                     <label for="genre">Genre:</label>
-                    <input type="text" class="form-control" placeholder="Enter genre" name="genre" id="genre">
+                    <input type="text" class="form-control" placeholder="Enter genre" name="genre" id="genre" value="{{ old('genre') }}">
                     <span class="help-block text-danger">{{$errors->first('genre')}}</span>
                 </div>
                 <div class="form-group">
                     <label for="location">Location:</label>
-                    <input type="text" class="form-control" placeholder="Enter location" name="location" id="location">
+                    <input type="text" class="form-control" placeholder="Enter location" name="location" id="location" value="{{ old('location') }}">
                     <span class="help-block text-danger">{{$errors->first('location')}}</span>
                 </div>
                 <div class="form-group">
@@ -60,7 +60,7 @@
                         <label class="input-group-btn my-0">
                             <span class="btn btn-large btn-outline-primary rounded-0" id="browse">
                                 Browse&hellip; 
-                            <input id="video-input" name="file" type="file" multiple>
+                            <input id="video-input" name="file" type="file" multiple value="{{ old('file') }}">
                             </span>
                         </label>
                         <input type="text" class="form-control rounded-0" readonly placeholder="Upload video">
@@ -73,7 +73,7 @@
                         <input class="form-check-input" type="checkbox" name="disclaimer" id="check_disclaimer"> Accept disclaimer
                     </label>
                 </div>
-                <button class="btn btn-primary">Upload</button>
+                <button class="btn btn-primary" id="btn_upload">Upload</button>
             </form>
         </div>
     </div>
@@ -100,6 +100,18 @@
             } else {
                 if (log) alert(log);
             }
+        });
+        $('#check_brand').click(function(){
+            if(($(this).is(":checked"))){
+                $('#band').val($('#name').val());
+            }
+            else{
+                $('#band').val("");
+            }
+        });
+        $('#check_disclaimer').click(function(){
+            // console.log($(this).is(":checked"));
+            $('#btn_upload').prop('disabled',$(this).is(":checked"));
         });
     });
     </script>
