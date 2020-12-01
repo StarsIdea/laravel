ba64eeff84d847f44a99b76e4d0b10ce572f0cd8
starsidea <director_9414@outlook.com>
2020-12-01 16:43:26 +0800

    modify

--- routes/web.php
+++ routes/web.php
@@ -161,14 +161,13 @@ Route::get('/admin/allow/{userType}/{id}','AdminController@allowed');
 Route::group([
     'middleware' => 'api',
     'prefix' => 'auth'
-
 ],
     function ($router) {
         Route::post('/login', 'AuthController@login');
-        Route::post('/register', [AuthController::class, 'register']);
-        Route::post('/logout', [AuthController::class, 'logout']);
-        Route::post('/refresh', [AuthController::class, 'refresh']);
-        Route::get('/user-profile', [AuthController::class, 'userProfile']);
+        Route::post('/register', 'AuthController@register');
+        Route::post('/logout', 'AuthController@logout');
+        Route::post('/refresh', 'AuthController@refresh');
+        Route::get('/user-profile', 'AuthController@userProfile');
     }
 );
 
