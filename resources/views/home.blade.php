b30c7af5810be2510cd5829567d85aaf251015a6
sergeykozindev <sergeykozindev@gmail.com>
2020-12-17 00:30:45 +0200

    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,9 +7,8 @@
 @section('content')
     <section>
         <h1>LiveShow</h1>
-        <p style="color: red !important; font-size: 36px; font-weight: bold;">Hi I think your telegram has some issue and you didn't reply at all. When you can see this text, please check your gmail and reply me.</p>
-        {{-- <p>LiveShow Cloud Stage - Launching Soon!<br />
-        Be the first to know!</p> --}}
+        <p>LiveShow Cloud Stage - Launching Soon!<br />
+        Be the first to know!</p>
     </section>
 
     <!-- Subscribe Form -->
